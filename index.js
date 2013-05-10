2e391bf0053b879ecabab375b130c5bd689326b0
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-05-10 13:31:38 -0700

    add min-stream version

--- index.js
+++ index.js
@@ -1,137 +1,65 @@
 var through = require('through')
-  , hashify = require('git-object-hash')
-  , apply = require('git-apply-delta')
-  , g2j = require('git-to-js')
-  , binary = require('bops')
+  , min = require('./min.js')
 
 module.exports = objectify
 
-var OFS_DELTA = 6
-  , REF_DELTA = 7
-
 function objectify(find) {
   var stream = through(write, end)
-    , loaded = []
-    , pending = 0
+    , inner = min(find)(read)
     , ended = false
+    , accum = []
+    , cb
 
-  stream.setMaxListeners(Infinity)
+  iter()
 
   return stream
 
-  function write(info) {
-    if(info.type === REF_DELTA) {
-      return object_from_ref_delta(info)
+  function iter() {
+    inner(null, function(err, data) {
+      if(err) {
+        stream.emit('error', err)
+        return
+      }
+      if(data === undefined) {
+        stream.queue(null)
+        return
+      }
+      stream.queue(data)
+      iter()
+    })
+  }
+
+  function write(buf) {
+    accum.push(buf)
+    if(!cb) {
+      return
     }
 
-    if(info.type === OFS_DELTA) {
-      return object_from_ofs_delta(info)
+    while(accum.length) {
+      cb(null, accum.shift())
     }
-
-    var out = {
-        offset: info.offset
-      , object: g2j(info.type, info.data)
-      , hash: null
-    }
-    out.object.hash = out.hash = hashify(out.object)
-    stream.queue(out.object)
   }
 
   function end() {
     ended = true
-    if(!pending) {
-      really_end()
-    }
+    console.log('ended')
+    if(cb) cb()
   }
 
-  function really_end() {
-    if(!ended) {
-      return
-    }
-    loaded = null
-    stream.queue(null)
-  }
-
-  function object_from_ofs_delta(info) {
-    // walk back from loaded.length to 0
-    var buf = info.reference
-      , _byte = buf[0]
-      , offset = _byte & 0x7F
-
-    while(_byte & 0x80) {
-      offset += 1
-      offset <<= 7
-      _byte = buf[idx++]
-      offset += _byte & 0x7F
-    }
-
-    for(var i = loaded.length - 1; i > -1; --i) {
-      var target = loaded[i]
-      if(target.offset === offset) {
-        break
-      } 
-    }
-
-    if(i === -1) {
-      stream.emit('error', new Error('could not find object at offset: '+offset))
-      return
-    }
-
-    apply_delta(info, target.object)
-  }
-
-  function object_from_ref_delta(info) {
-    var hash = binary.to(info.reference, 'hex')
-      , found = false
-
-    for(var i = loaded.length - 1; i > -1; --i) {
-      var target = loaded[i]
-      if(target.hash === hash) {
-        break
-      } 
-    }
-
-    if(i === -1) {
-      ++pending
-      var listener = function(data) {
-        if(data.hash === hash) {
-          delayed_find(data)
-        }
+  function read(close, callback) {
+    if(close) {
+      if(close === true) { 
+        stream.queue(null)
+      } else {
+        stream.emit('error', close)
       }
-      stream.once('data', listener)
-      return find(info.reference, function(err, data) {
-        if(found) {
-          return
-        }
-        stream.removeListener('data', listener)
-        if(data) {
-          delayed_find(data) 
-        }
-      })
+      return
     }
 
-    return apply_delta(info, target.object)
-
-    function delayed_find(data) {
-      found = true
-      info.type = data.type
-
-      apply_delta(info, data)
-      !--pending && really_end()
-    }
-  }
-
-  function apply_delta(info, target) {
-    var new_data = apply(info.data, target.serialize())
-      , out = {}
-
-    var out = {
-        offset: info.offset
-      , object: g2j(target.type, new_data)
-      , hash: null
+    if(ended) {
+      return callback()
     }
 
-    out.object.hash = out.hash = hashify(out.object)
-    stream.queue(out.object)
+    cb = callback
   }
 }
---
+++ min.js
@@ -0,0 +1,142 @@
+var hashify = require('git-object-hash')
+  , apply = require('git-apply-delta')
+  , g2j = require('git-to-js')
+  , binary = require('bops')
+
+module.exports = objectify
+
+var OFS_DELTA = 6
+  , REF_DELTA = 7
+
+function objectify(find) {
+  var waiting = {} 
+    , traced = []
+    , emit
+
+  var offset_listeners = []
+    , offset_to_ref = []
+    , pending = 0
+    , ended = false
+
+  return function inner_objectify(read) {
+    return function(close, _emit) {
+      emit = _emit
+      if(close) {
+        traced =
+        offset_listeners =
+        offset_to_ref =
+        waiting = null
+        return close === true ? emit() : emit(err)
+      }
+
+      read(null, onread)
+    }
+  }
+
+  function onread(err, info) {
+    if(err) {
+      return emit(err)
+    }
+
+    if(!info) {
+      ended = true
+      if(!pending) emit()
+      return
+    }
+
+    if(info.type === REF_DELTA) {
+      return object_from_ref_delta(info)
+    }
+
+    if(info.type === OFS_DELTA) {
+      return object_from_ofs_delta(info)
+    }
+
+    var object = g2j(info.type, info.data)
+    object.hash = hashify(object)
+    do_emit(object, info.offset)
+  }
+
+  function object_from_ofs_delta(info) {
+    var buf = info.reference
+      , _byte = buf[0]
+      , offset = _byte & 0x7F
+      , ref
+
+    ++pending
+    while(_byte & 0x80) {
+      offset += 1
+      offset <<= 7
+      _byte = buf[idx++]
+      offset += _byte & 0x7F
+    }
+
+    offset = info.offset - offset
+    ref = offset_to_ref[offset]
+
+    if(ref) {
+      return find(ref, function(err, obj) {
+        resolve_delta(err, info, obj)
+      })
+    }
+    
+    var listener = offset_listeners[offset] = offset_listeners[offset] || []
+    listener[listener.length] = info
+  }
+
+  function object_from_ref_delta(info) {
+    var hash = binary.to(info.reference, 'hex')
+      , idx = add_wait(hash, info)
+
+    ++pending
+    if(has_emitted(hash)) {
+      return find(hash, function(err, obj) {
+        if(!obj) return
+
+        if(waiting[hash]) {
+          waiting[hash].splice(waiting[hash].indexOf(info), 1)
+        }
+        resolve_delta(err, info, obj)
+      })
+    }
+  }
+
+  function resolve_delta(err, info, object) {
+    var new_data = apply(info.data, object.serialize())
+      , new_object = g2j(object.type, new_data)
+
+    new_object.hash = hashify(new_object)
+    do_emit(new_object, info.offset)
+
+    --pending
+    if(ended) {
+      emit()
+    }
+  }
+
+  function has_emitted(hash) {
+    return hash in waiting
+  }
+
+  function add_wait(hash, info) {
+    waiting[hash] = waiting[hash] || []
+    waiting[hash].push(info)
+    return waiting[hash].length - 1
+  }
+
+  function do_emit(obj, offset) {
+    var waiting_list = waiting[obj.hash]
+
+    if(waiting_list) while(waiting_list.length) {
+      resolve_delta(null, waiting_list.shift(), obj)
+    }
+
+    // make sure we pick up any ofs delta listeners as well.
+    var at_offset = offset_listeners[offset]
+    if(at_offset) while(at_offset.length) {
+      resolve_delta(null, at_offset.shift(), obj)
+    }
+    offset_to_ref[offset] = obj.hash
+    emit(null, obj)
+  }
+}
